d5bf43bb1f7eb713c54242ddc4b331c615130900
AntonioALino <antonioaplino@gmail.com>
2024-11-17 15:27:16 -0300

    feat: express inital configs

---
+++ back/src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+import { config } from 'dotenv'
+
+config();
+
+const app = express();
+
+const port = process.env.PORT || 8000
+
+app.listen(port, () => console.log(`listen on port ${port}!`));
+
+app.get("/", (req, res) => {
+    res.send("hello");
+})
